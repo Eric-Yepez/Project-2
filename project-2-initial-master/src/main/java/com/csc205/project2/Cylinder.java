3e5f8df8cde6c60a243ae0120dc0c6bcfdd86b03
Eric-Yepez <eri2260169@maricopa.edu>
2022-02-24 15:45:51 -0700

    Initial commit

--- project-2-initial-master/src/main/java/com/csc205/project2/Cylinder.java
+++ project-2-initial-master/src/main/java/com/csc205/project2/Cylinder.java
@@ -10,5 +10,37 @@ public class Cylinder {
         this.height = height;
     }
 
-    
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    double surfaceArea() {
+        return 2.0*22/7*radius*(radius + height);
+    }
+
+    double volume() {
+        return 22/7*radius*radius*height;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Cylinder {");
+        sb.append("radius=").append(radius);
+        sb.append(", surface area=").append(surfaceArea());
+        sb.append(", volume=").append(volume());
+        sb.append('}');
+        return sb.toString();
+    }
 }
